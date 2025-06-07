51f2f83261cff63a6081c22cb7d2a9fd08bda1c3
Tos <thomas.chan@tufts.edu>
2025-06-06 23:22:29 -0400

    Correct read.xlsx parameter name

--- functions/plot_dotplot.R
+++ functions/plot_dotplot.R
@@ -15,7 +15,7 @@ blues_hex <- apply(blues_colormap, 1, function(row) {
 })
 
 plot_dotplot <- function(lin_gex, lin_cite, lin, widths, cluster_order) {
-    dotplot_markers <- read.xlsx('/projects/home/tlchan/projects/ascites/figure_panels/data/dotplot_markers.xlsx', sheet = toupper(lin))
+    dotplot_markers <- read.xlsx('/projects/home/tlchan/projects/ascites/figure_panels/data/dotplot_markers.xlsx', sheetName = toupper(lin))
 
     lin_genes <- dotplot_markers %>%
         filter(!is.na(genes)) %>%
